8859242084a7b0cdc7b123b2b1765e5a4a65b568
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-12-23 18:10:42 +0800

    refactor

--- src/bootstrap.pas
+++ src/bootstrap.pas
@@ -16,6 +16,11 @@ uses
 type
 
     TAppServiceProvider = class(TDaemonAppServiceProvider)
+    protected
+        function buildDispatcher(
+            const ctnr : IDependencyContainer;
+            const routeMatcher : IRouteMatcher
+        ) : IDispatcher; override;
     public
         procedure register(const container : IDependencyContainer); override;
     end;
@@ -47,8 +52,44 @@ uses
     AuthOnlyMiddlewareFactory;
 
 
-    procedure TAppServiceProvider.register(const container : IDependencyContainer);
+    function TAppServiceProvider.buildDispatcher(
+        const ctnr : IDependencyContainer;
+        const routeMatcher : IRouteMatcher
+    ) : IDispatcher;
     var config : IAppConfiguration;
+    begin
+        ctnr.add(
+            'config',
+            TJsonFileConfigFactory.create(
+                getCurrentDir() + '/config/config.json'
+            )
+        );
+        config := ctnr.get('config') as IAppConfiguration;
+        ctnr.add('appMiddlewares', TMiddlewareListFactory.create());
+
+        ctnr.add(
+            'sessionManager',
+            TJsonFileSessionManagerFactory.create(
+                config.getString('session.name'),
+                config.getString('session.dir')
+            )
+        );
+
+        ctnr.add(
+            GuidToString(IDispatcher),
+            TSessionDispatcherFactory.create(
+                ctnr.get('appMiddlewares') as IMiddlewareLinkList,
+                routeMatcher,
+                TRequestResponseFactory.create(),
+                ctnr.get('sessionManager') as ISessionManager,
+                (TCookieFactory.create()).domain(config.getString('cookie.domain')),
+                config.getInt('cookie.maxAge')
+            )
+        );
+        result := ctnr.get(GuidToString(IDispatcher)) as IDispatcher;
+    end;
+
+    procedure TAppServiceProvider.register(const container : IDependencyContainer);
     begin
         {$INCLUDE Dependencies/dependencies.inc}
     end;
